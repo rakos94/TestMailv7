0654f33d625f7e4bf0d29f6d7de9e6fd30f8a333
kamal <rakos94@gmail.com>
2020-10-30 15:25:43 +0700

    Add initial project

---
+++ resources/views/name.blade.php
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        Testing Email
+    </body>
+</html>
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Mail\VerifyMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('/test/email', function(){
+    // send email
+    Mail::to('rakos94@gmail.com')
+        ->send(new VerifyMail());
+    return;
+});
